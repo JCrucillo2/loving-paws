f67508122d6d83a1b7138620478884f79e55a7b8
JCrucillo2 <junnelcrucillo729@outlook.com>
2022-12-05 20:12:41 -0700

    First commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lovingpaws
+ */
+
+?>
+
+<footer id="colophon" class="site-footer">
+	<div>
+		<h3>&copy; Hello</h3>
+	</div>
+</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
